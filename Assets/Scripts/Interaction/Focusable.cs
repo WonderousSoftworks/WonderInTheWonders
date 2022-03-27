854b95b53d2d65562ab8730b1e22f50a648722df
Jamee Kim <jameekim@nodaplife.me>
2022-03-27 04:41:38 -0400

    Implement basic focusable with input actions

---
+++ Assets/Scripts/Interaction/Focusable.cs
@@ -0,0 +1,35 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace Interaction
+{
+    [DisallowMultipleComponent]
+    public class Focusable : MonoBehaviour
+    {
+        [Header("Zoom-in focus control")]
+        [SerializeField]
+        private CinemachineVirtualCamera focusCamera;
+        [SerializeField]
+        private int focusPriority = 20;
+        [SerializeField]
+        private int unfocusPriority = 0;
+
+        public void GetFocus()
+        {
+            if (focusCamera.Priority == focusPriority)
+                return;
+
+            focusCamera.Priority = focusPriority;
+            Debug.Log($"{name} is getting your attention!!");
+        }
+
+        public void RemoveFocus()
+        {
+            if (focusCamera.Priority == unfocusPriority)
+                return;
+
+            focusCamera.Priority = unfocusPriority;
+            Debug.Log($"{name} is not interesting anymore :(");
+        }
+    }
+}
---
+++ Assets/Scripts/Interaction/PlayerFocusController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Interaction
+{
+    [DisallowMultipleComponent]
+    public class PlayerFocusController : MonoBehaviour
+    {
+        public Focusable tmpFocus;
+
+        public void OnMoveFocus(InputAction.CallbackContext context)
+        {
+            if (!context.performed) return;
+
+            if (context.ReadValue<float>() > 0)
+                tmpFocus.GetFocus();
+            else
+                tmpFocus.RemoveFocus();
+        }
+    }
+}
