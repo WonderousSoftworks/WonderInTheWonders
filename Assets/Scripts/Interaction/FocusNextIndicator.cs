be451ff4ecffd263b471e1ff46e06908629f5bfc
Jamee Kim <jameekim@nodaplife.me>
2022-04-12 23:15:53 -0400

    Implement basic gameplay ui

---
+++ Assets/Scripts/Interaction/FocusNextIndicator.cs
@@ -0,0 +1,50 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FocusNextIndicator : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField]
+    private Image arrow;
+
+    [SerializeField]
+    private TMP_Text label;
+
+    [SerializeField]
+    private TMP_Text displayName;
+
+    [SerializeField]
+    private TMP_Text key;
+
+    [Header("Settings")]
+    [SerializeField]
+    private Color activeColor = Color.white;
+
+    [SerializeField]
+    private Color inactiveColor = Color.gray;
+
+    private string defaultLabel;
+
+    private void Awake()
+    {
+        // Save the original text
+        defaultLabel = label.text;
+
+        ShowState(null);
+    }
+
+    public void ShowState(Focusable focusable)
+    {
+        bool isActive = focusable != null;
+
+        Color color = isActive ? activeColor : inactiveColor;
+        arrow.color = color;
+        label.color = color;
+        displayName.color = color;
+        key.color = color;
+
+        label.text = isActive ? defaultLabel : "Exit";
+        displayName.text = isActive ? focusable.Data.DisplayName : "";
+    }
+}
---
+++ Assets/Scripts/Spaceship/BoostGauge.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BoostGauge : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform inner;
+
+    [SerializeField]
+    private Image backgroundImage;
+
+    [SerializeField]
+    private Image barImage;
+
+    public float Value
+    {
+        get => currentValue;
+        private set
+        {
+            currentValue = Mathf.Clamp01(value);
+            Vector2 currentAnchorMax = inner.anchorMax;
+            currentAnchorMax.x = currentValue;
+            inner.anchorMax = currentAnchorMax;
+        }
+    }
+    private float currentValue;
+
+    public void OnBoostAmountChanged(float current, float max)
+    {
+        Value = current / max;
+    }
+
+    public void OnFocusStateChanged(PlayerFocusController.FocusStateInfo focusState)
+    {
+        backgroundImage.gameObject.SetActive(focusState.current == null);
+    }
+}
